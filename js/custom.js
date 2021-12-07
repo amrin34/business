8882d50735cb96400f61db59c9f3a6e944881c69
amrin34 <amrinsaba3409@gmail.com>
2021-12-07 12:20:20 +0600

    justadd

--- js/custom.js
+++ js/custom.js
@@ -20,12 +20,28 @@ $(document).ready(function(){
 
     $('.venobox').venobox(); 
 
+
+
+      //for testimonial-slider
+    
+        $('.testimonial-slider').slick({
+            dots: true,
+            arrows: false,
+            slidesToShow: 2,
+        });
+
+    //funfact counter
+        $('.counter').counterUp();
+
+
+        $('.brand-slider').slick({
+            arrows: true,
+            slidesToShow: 5,
+            centerMode: true,
+            centerPadding:'0px',
+            prevArrow: '<i class="fas fa-chevron-left prev-arrow"></i>',
+            nextArrow: '<i class="fas fa-chevron-right next-arrow"></i>',
+        });
+
 });
 
-//for testimonial-slider
-    
-$('.testimonial-slider').slick({
-    dots: true,
-    arrows: false,
-    slidesToShow: 2,
-});
